5b9509619f42453ee79259910be4acd1a1ac0460
4mirhoseinhasani <abaiat920@gmail.com>
2025-04-13 16:05:18 +0330

    bug fix!

--- new_course/Conditional_store.py
+++ new_course/Conditional_store.py
@@ -2,7 +2,7 @@ purchase_amount = int(input())
 if purchase_amount > 50000:
     purchase_amount = purchase_amount * 0.8
     print(int(purchase_amount))
-elif 2000 < purchase_amount < 50000:
+elif 20000 < purchase_amount < 50000:
     purchase_amount = purchase_amount * 0.9
     print(int(purchase_amount))
 elif purchase_amount < 20000:
