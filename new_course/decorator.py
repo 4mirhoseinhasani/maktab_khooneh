6e1677705efcbf60b069fd6d6832d72358e1b908
4mirhoseinhasani <abaiat920@gmail.com>
2025-04-24 14:14:12 +0330

    decoretor in python

---
+++ new_course/decorator.py
@@ -0,0 +1,27 @@
+"""
+Write a decorator that calculates the execution time of a function and displays it after the function is executed. Then, use this decorator for a function that creates a list from 1 to n.
+
+Input:
+The number n that specifies the size of the list.
+
+Output:
+The function's return value (list 1 to n)
+The execution time of the function in seconds
+"""
+
+import timeit
+def run_time(function):
+    def wrraper():
+        timer = timeit.Timer(function).timeit(1)
+        print(f"Time taken: {timer:.4f} seconds")
+    return wrraper
+
+@run_time
+def make_list():
+    list = []
+    n = int(input("please enter a number: "))
+    for n in range(1, n+1):
+        list.append(n)
+    return list
+
+make_list()
